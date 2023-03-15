0e267ff5ac3005d25764f88948a75c3bdbde19a4
coderchrisdean <coderchrisdean@gmail.com>
2023-03-15 12:02:52 -0700

    completed todos for webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. ✔️
+// TODO: Add CSS loaders and babel to webpack. ✔️
 
 module.exports = () => {
   return {
@@ -22,7 +23,28 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new MiniCssExtractPlugin(),      
+      new MiniCssExtractPlugin(),     
+      new WebpackPwaManifest({
+        name: 'just another text editor',
+        short_name: 'JATE',
+        description: 'PWA for JATE.  Text editor to use offline or online.',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'), //path to logo/image
+            sizes: [96,128,192,256,384,512], // logo/image sizes
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src/js/src-sw.js', //service worker file
+        swDest: 'service-worker.js',  
+        exclude: []
+      })
     ],
 
     module: {
@@ -42,6 +64,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
